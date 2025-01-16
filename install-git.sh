40171d76e562c443d355b7890f452ae3b9d19774
Igor Martins <igor.rmartins@outlook.com>
2025-01-16 20:22:13 -0300

    Update install-git.sh

--- install-git.sh
+++ install-git.sh
@@ -2,6 +2,8 @@
 
 cd /tmp
 git clone https://github.com/Donwaztok/hyprland-dotfiles.git
+mkdir ~/.config
+cp hyprland-dotfiles/* ~/.config
 cd hyprland-dotfiles
 sudo chmod +x install.sh
 ./install.sh
